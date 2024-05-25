93cd04cbfd8c45e02982cadb8541e41517301dce
Valentino Olmedo <valenolme@gmail.com>
2024-05-25 13:49:23 -0300

    git commit -m "segundo commit"

--- Program.cs
+++ Program.cs
@@ -1,16 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-using System;
-namespace Programa
-{
-    internal class Program{
-        static void main(string[] args){
-            Console.WriteLine("Hello, World!");
-            int a;
-            int b;
-            a=10;
-            b=a;
-            Console.WriteLine("valor de a:"+a);
-            Console.WriteLine("valor de b:"+b);
-        }
+﻿Console.WriteLine("Escriba un numero para invertir");
+int cifras=0;
+int num = int.Parse(Console.ReadLine());
+if(num>=0){
+    int aux=num;
+    while(aux>0){
+        aux/=10;
+        cifras++;
     }
+    int aux1;
+    int num1=0;
+    while(num>0){
+        aux1=num%10;
+        num1+=aux1*(int)(Math.Pow(10.0,cifras-1));
+        cifras--;
+        num/=10;
+    }
+    num=num1;
+    Console.WriteLine("El numero invertido es: "+num);
+}else{
+    Console.WriteLine("El numero no es valido, es menor a 0");
 }
