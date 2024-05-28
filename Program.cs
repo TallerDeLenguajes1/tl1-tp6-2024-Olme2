899b7cc4fe2ea0c67114fa1cc6864f5bddc9ac5c
Valentino Olmedo <valenolme@gmail.com>
2024-05-27 21:58:41 -0300

    Revert "cuarto commit"
    This reverts commit 562691b9a55a8a2ff8ae4914d947df77649ff661.

--- Program.cs
+++ Program.cs
@@ -1,19 +1,40 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System.ComponentModel.DataAnnotations;
-using System.Reflection.Emit;
-Console.WriteLine("Ingrese un numero mayor o igual a 0");
-float num;
-do{
-    num=float.Parse(Console.ReadLine());
-}while(num<0);
-Console.WriteLine("Valor absoluto: "+Math.Abs(num));
-Console.WriteLine("Cuadrado: "+Math.Pow(num,2));
-Console.WriteLine("Raiz Cuadrada: "+Math.Sqrt(num));
-Console.WriteLine("Seno: "+Math.Sin(num));
-Console.WriteLine("Coseno: "+Math.Cos(num));
-Console.WriteLine("Parte entera de un float: "+Math.Truncate(num));
-Console.WriteLine("Ingrese dos numeros: ");
-float num1=float.Parse(Console.ReadLine());
-float num2=float.Parse(Console.ReadLine());
-Console.WriteLine("Maximo numero: "+Math.Max(num1,num2));
-Console.WriteLine("Minimo numero: "+Math.Min(num1,num2));
+int hacer=1;
+while(hacer==1){
+Console.WriteLine("CALCULADORA\n ELIJA UNA OPERACION\n1:SUMA\n2:RESTA\n3:MULTIPLICACION:\n4:DIVISION");
+int opcion=int.Parse(Console.ReadLine());
+while(opcion<1 || opcion>4){
+    Console.WriteLine("Elija una opcion correcta");
+    opcion=int.Parse(Console.ReadLine());
+}
+Console.WriteLine("Escriba los dos numeros para operar");
+float a;
+float b;
+if(opcion==4){
+    a=float.Parse(Console.ReadLine());
+    b=float.Parse(Console.ReadLine());
+    while(b==0){
+        Console.WriteLine("El segundo numero debe ser distinto de cero, reescribalo");
+        b=float.Parse(Console.ReadLine());
+    }
+}else{
+    a=float.Parse(Console.ReadLine());
+    b=float.Parse(Console.ReadLine());
+}
+    switch (opcion){
+        case 1:
+            Console.WriteLine("El resultado de su suma es:"+(a+b));
+        break;
+        case 2:
+            Console.WriteLine("El resultado de su resta es:"+(a-b));
+        break;
+        case 3:
+            Console.WriteLine("El resultado de su multiplicacion es:"+(a*b));
+        break;
+        case 4:
+            Console.WriteLine("El resultado de su division es:"+(a/b));
+        break;
+    }
+    Console.WriteLine("¿Desea hacer otra operacion?\n1: SI\n2: NO");
+    hacer=int.Parse(Console.ReadLine());
+}
